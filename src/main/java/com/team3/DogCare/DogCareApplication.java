9e040324345f996d43d10c93ff6a629963e3cac7
KYJ <https://github.com/kwonyung>
2023-10-10 11:01:32 +0900

    초기설정

---
+++ src/main/java/com/team3/DogCare/DogCareApplication.java
@@ -0,0 +1,13 @@
+package com.team3.DogCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3/DogCare/DogCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.DogCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
