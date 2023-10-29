2342f85867745b05fb557ebdf7833c627e2e4fcf
iden seo <epik35@naver.com>
2023-10-29 13:41:16 +0900

    배포 테스트

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter, Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
 );
